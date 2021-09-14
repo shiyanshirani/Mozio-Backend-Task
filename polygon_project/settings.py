694315a2a445a659f05adf5ffdc26ec0c98e1bd1
shiyanshirani <shiyan99s@gmail.com>
2021-09-14 13:34:08 +0530

    added static root

--- polygon_project/settings.py
+++ polygon_project/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY="django-insecure-=6mc2c+tkd7h2i1)fldr$8d#b(ef7ns%5y_w)e#p#dle6fx3ix"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
